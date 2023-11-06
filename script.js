30ed8723ce808c508620bfcb6c64bdf4127da6e5
Qeleran <leka.false@gmail.com>
2023-11-06 23:24:57 +0600

    Add project

---
+++ script.js
@@ -0,0 +1,36 @@
+var a = 1;
+document.getElementById("button").onclick = function () {
+  a = a + 1;
+  if (a % 2 == 0) {
+    document.getElementById("button").style.backgroundColor =
+      "rgb(233, 233, 233)";
+    document.getElementById("bt-in").style.backgroundColor = "rgb(58, 58, 58)";
+    document.getElementById("bt-in").style.marginLeft = "56%";
+    document.getElementById("experimental-zone").style.backgroundColor =
+      "rgb(49, 49, 49)";
+    document.getElementById("bt-in").innerHTML = "Black";
+    document.getElementById("bt-in").style.color = "white";
+
+    document.getElementById("audio3").currentTime = "0";
+    document.getElementById("audio3").play();
+
+    // document.getElementById("audio").play();
+    // document.getElementById("audio2").pause();
+  } else {
+    a = 1;
+    document.getElementById("button").style.backgroundColor = "rgb(58, 58, 58)";
+    document.getElementById("bt-in").style.backgroundColor =
+      "rgb(233, 233, 233)";
+    document.getElementById("bt-in").style.marginLeft = "0";
+    document.getElementById("experimental-zone").style.backgroundColor =
+      "rgb(209, 209, 209)";
+    document.getElementById("bt-in").innerHTML = "White";
+    document.getElementById("bt-in").style.color = "black";
+
+    document.getElementById("audio3").currentTime = "0";
+    document.getElementById("audio3").play();
+
+    // document.getElementById("audio2").play();
+    // document.getElementById("audio").pause();
+  }
+};
